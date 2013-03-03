77c8ae13cff07c950714057f06881af5afb97d67
jmarsico <jmarsico@gmail.com>
2013-03-03 14:07:38 -0500

    adding testApp.cpp

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,7 +1,14 @@
 #include "testApp.h"
 
+int conetop = 0;
+
+
 //--------------------------------------------------------------
 void testApp::setup(){
+    ofEnableSmoothing();
+    ofEnableAlphaBlending();
+    ofBackground(0);
+    ofBackgroundGradient(ofColor::black, ofColor::aquamarine, OF_GRADIENT_CIRCULAR);
 
 }
 
@@ -12,6 +19,35 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    
+    //center circle
+    ofSetColor(0, 0, 255);
+    ofFill();
+    ofCircle(ofGetWindowWidth()/2, ofGetWindowHeight()/2, 100);
+    
+    //curve
+    ofNoFill();
+    ofSetColor(ofColor::aquamarine);
+    ofCurve(10, 100, 30, 10, 700, 700, 200, 430);
+   
+
+    //outlined cone
+
+    ofFill();
+    ofSetColor(ofColor::coral);
+    ofCone(300, 600, 10, 60, 100);
+    ofNoFill();
+    ofSetColor(ofColor::burlyWood);
+    ofCone(300, 600, 10, 60, 100);
+    
+    
+    //triangle
+    ofFill();
+    ofSetColor(200, 30, 30, 100);    
+    ofTriangle(ofGetWindowWidth()*0.5, 10, 10, ofGetWindowHeight() - 10, ofGetWindowWidth() - 10, ofGetWindowHeight() - 10);
+    
+    
+    
 
 }
 
